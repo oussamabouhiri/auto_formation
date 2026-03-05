8fc5d0f3821643fa4dd9a02f49821fbaade1fcda
Oussama bouhiri <oussamabouhiri@gmail.com>
2026-03-05 03:58:39 +0000

    ticket systyme challenge

---
+++ ticket_system.php
@@ -0,0 +1,22 @@
+<?php
+
+$age = 10;
+$price = 0;
+
+if ($age < 12) {
+    $price = 20;
+} elseif ($age >= 12 && $age <= 18) {
+    $price = 40;
+} elseif ($age > 60) {
+    $price = 30;
+} else {
+    $price = 60;
+}
+
+echo "Ticket price: " . $price . " DH\n";
+
+if ($price == 20) {
+    echo "Special: Children's Menu included!";
+}
+
+?>
