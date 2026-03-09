1afa76236979fbf2ea04cc83bed2d23ec4c22661
Oussama bouhiri <oussamabouhiri@gmail.com>
2026-03-09 09:36:52 +0000

    second challenge is finished

---
+++ functionsChallenges/index02.php
@@ -0,0 +1,10 @@
+<?php
+
+function calculateArea($width,$height){
+    $totalArea=0;
+    return $totalArea+=($width*$height);
+}
+$result = calculateArea(40,5.5);
+echo "The total area is ".$result ." ". "square units."."\n";
+
+?>
